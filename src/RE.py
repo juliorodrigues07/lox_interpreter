ee543aedff00d8ce09c32c08ac0a4b2f78c1a05e
Paulovf10 <p.victorsousapro@gmail.com>
2022-09-14 17:07:48 -0300

    ER

--- src/RE.py
+++ src/RE.py
@@ -1,10 +1,12 @@
 import re
 
-num = "^(0(,\d{0,2})?|-?[1-9]\d*(,\d{1,20})?|-0,(0[1-9]|[1-9]\d?))$"
-id = "^[a-zA-Z_]+[a-zA-Z0-9_]*"
+numeros = "^(0(,\d{0,2})?|-?[1-9]\d*(,\d{1,20})?|-0.(0[1-9]|[1-9]\d?))$"
+indentificadores = "^[a-zA-Z_]+[a-zA-Z0-9_]*"
 
 b = '123123,666666'
-c = '123c123'
+c = '1asda'
 
-if re.findall(id, b):
-    print('sim')
+if re.findall(numeros, b):
+    print('numeros')
+elif re.findall(indentificadores, b):
+    print('identificadores')
