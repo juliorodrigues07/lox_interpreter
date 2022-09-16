b7573d9bf034479cec861badfc00fcdd8fca91a4
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2022-09-15 21:08:07 -0300

    feat: Adicionados tratamentos para tokens com mais de um caractere e para numerais

--- src/scanner.py
+++ src/scanner.py
@@ -44,8 +44,43 @@ class Scanner:
             self.add_token(TokenType.SEMICOLON)
         elif c == '*':
             self.add_token(TokenType.STAR)
+        elif c == '!':
+            if self.match('='):
+                self.add_token(TokenType.BANG_EQUAL)
+            else:
+                self.add_token(TokenType.BANG)
+        elif c == '=':
+            if self.match('='):
+                self.add_token(TokenType.EQUAL_EQUAL)
+            else:
+                self.add_token(TokenType.EQUAL)
+        elif c == '<':
+            if self.match('='):
+                self.add_token(TokenType.LESS_EQUAL)
+            else:
+                self.add_token(TokenType.LESS)
+        elif c == '>':
+            if self.match('='):
+                self.add_token(TokenType.GREATER_EQUAL)
+            else:
+                self.add_token(TokenType.GREATER)
+        elif c == '/':
+            if self.match('/'):
+                while self.peek() != '\n' and not self.is_at_end():
+                    self.advance()
+            else:
+                self.add_token(TokenType.SLASH)
+        elif c == ' ' or c == '\r' or c == '\t':
+            pass
+        elif c == '\n':
+            self.line += 1
+        elif c == '"':
+            self.string()
         else:
-            print(str(self.line) + 'Unexpected character!' + str(c))
+            if self.is_digit(c):
+                self.number()
+            else:
+                print(str(self.line) + 'Unexpected character!' + str(c))
 
     def is_at_end(self):
 
@@ -56,6 +91,64 @@ class Scanner:
 
         self.current += 1
         return self.source[self.current + 1:]
+
+    def match(self, expected):
+
+        if self.is_at_end():
+            return False
+
+        if self.source[self.current] != expected:
+            return False
+
+        self.current += 1
+        return True
+
+    def peek(self):
+
+        if self.is_at_end():
+            return '\0'
+
+        return self.source[self.current]
+
+    def string(self):
+
+        while self.peek() != '"' and not self.is_at_end():
+            if self.peek() == '\n':
+                self.line += 1
+                self.advance()
+
+        if self.is_at_end():
+            print(str(self.line) + 'Unexpected character!' + self.peek())
+            return
+
+        self.advance()
+        string_value = self.source[self.start + 1 : self.current - 1]
+        self.add_token(TokenType.STRING, string_value)
+
+    @static
+    def is_digit(self, c):
+
+        return '0' <= c <= '9'
+
+    def number(self):
+
+        while self.is_digit(self.peek()):
+            self.advance()
+
+        if self.peek() == '.' and self.is_digit(self.peek_next()):
+            self.advance()
+
+            while self.is_digit(self.peek()):
+                self.advance()
+
+        self.add_token(TokenType.NUMBER, float(self.source[self.start, self.current]))
+
+    def peek_next(self):
+
+        if self.current + 1 >= len(self.source):
+            return '\0'
+
+        return self.source[self.current + 1]
 
     def add_token(self, token_type: TokenType):
         self.add_token(token_type, None)
