86e0b49daa798436907503c49dc698ab94670632
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2022-10-18 19:12:22 -0300

    feat: Adicionado relatório para erros na análise sintática (expressões)

--- src/error_handler.py
+++ src/error_handler.py
@@ -1,3 +1,6 @@
+from token import Token
+from token_type import TokenType
+
 
 class ErrorHandler:
 
@@ -5,8 +8,14 @@ class ErrorHandler:
         self.had_error = False
 
     def error(self, line, message):
-
         self.report(line, '', message)
+
+    def error_parser(self, token: Token, msg):
+
+        if token.token_type == TokenType.EOF:
+            self.report(token.line, ' at end', msg)
+        else:
+            self.report(token.line, ' at ', + token.lexeme + ' ' + msg)
 
     def report(self, line, where, message):
 
